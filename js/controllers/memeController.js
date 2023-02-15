ef5a8f5bb4b6ef3e9be24617657f7bbe117f4866
rania <raniakittan@hotmail.com>
2023-02-15 15:39:15 +0200

    test

--- js/controllers/memeController.js
+++ js/controllers/memeController.js
@@ -4,6 +4,10 @@ const gCtx = gCanvas.getContext('2d')
 let gCurrMeme = {}
 let gCurrSelections = {}
 
+// document.querySelector('.meme-text').addEventListener('keyup', function (ev) {
+//   if (ev.keyCode === 13) onAddTxt()
+// })
+
 function drawImgOnCanvas(image) {
   let img = new Image()
   img.onload = () => {
@@ -55,64 +59,82 @@ function onWriteTxt(el) {
   onAddTxt()
 }
 
-function onAddTxt(isReRender = false) {
-  let txt
-  if (!isReRender) {
-    if (gCurrMeme.currTxtIdx === 1)
-      gCurrSelections.y = gCanvas.height - gCurrSelections.fontSize + 50
-    else if (gCurrMeme.currTxtIdx > 1) gCurrSelections.y = gCanvas.height / 2
-    const { txt: text, fontSize, font } = getTxtInfo()
-    txt = text
-    gCurrMeme.txts.push({
-      txtIdx: gCurrMeme.currTxtIdx++,
-      text: txt,
-      x: gCurrSelections.x,
-      y: gCurrSelections.y,
-      stroke: gCurrSelections.stroke,
-      fill: gCurrSelections.fill,
-      align: gCurrSelections.align,
-      font: font,
-      fontSize: fontSize,
-    })
-  } else {
-    txt = gCurrSelections.text
+function onAddTxt(fillColor, strokeColor, isReRender = false) {
+    let txt
+    if (!isReRender) {
+      if (gCurrMeme.currTxtIdx === 1)
+        gCurrSelections.y = gCanvas.height - gCurrSelections.fontSize + 50
+      else if (gCurrMeme.currTxtIdx > 1) gCurrSelections.y = gCanvas.height / 2
+      const { txt: text, fontSize, font } = getTxtInfo()
+      txt = text
+      gCurrMeme.txts.push({
+        txtIdx: gCurrMeme.currTxtIdx++,
+        text: txt,
+        x: gCurrSelections.x,
+        y: gCurrSelections.y,
+        stroke: strokeColor,
+        fill: fillColor,
+        align: gCurrSelections.align,
+        font: font,
+        fontSize: fontSize,
+      })
+    } else {
+      txt = gCurrSelections.text
+    }
+    const { fontSize, font } = getTxtInfo()
+    gCtx.lineWidth = '2'
+    gCtx.strokeStyle = strokeColor
+    gCtx.fillStyle = fillColor
+    gCtx.font = `${fontSize}px ${font}`
+    gCtx.textAlign = gCurrSelections.align
+    drawText(
+      gCurrSelections.x,
+      gCurrSelections.y,
+      fontSize,
+      fillColor,
+      txt,
+      font
+    )
+    document.querySelector('.meme-text').value = ''
   }
-  const { fontSize, font } = getTxtInfo()
-  gCtx.lineWidth = '2'
-  gCtx.strokeStyle = gCurrSelections.stroke
-  gCtx.fillStyle = gCurrSelections.fill
-  gCtx.font = `${fontSize}px ${font}`
-  gCtx.textAlign = gCurrSelections.align
-  drawText(
-    gCurrSelections.x,
-    gCurrSelections.y,
-    fontSize,
-    gCurrSelections.fill,
-    txt,
-    font
-  )
-  document.querySelector('.meme-text').value = ''
-}
-
+  
 //move later
 
 function getTxtInfo() {
   const txt = document.querySelector('.meme-text').value
   const font = document.querySelector('.change-font').value
   const fontSize = parseInt(
-    document.querySelector('.font-size').innerText.slice(11)
+    document.querySelector('.font-size').innerText
   )
-  return { txt, fontSize, font }
+  return { txt, fontSize, font}
 }
 
 function drawText(x, y, size, color, txt, font) {
   gCtx.lineWidth = 1
-  gCtx.strokeStyle = 'brown'
   gCtx.fillStyle = color
   gCtx.font = `${size}px ${font}`
   gCtx.textAlign = 'center'
   gCtx.textBaseline = 'middle'
 
-  gCtx.fillText(txt, x, y) // Draws (fills) a given text at the given (x, y) position.
-  gCtx.strokeText(txt, x, y) // Draws (strokes) a given text at the given (x, y) position.
+  gCtx.fillText(txt, x, y) 
+  gCtx.strokeText(txt, x, y) 
 }
+
+function onChangeFill(el) {
+    gCurrSelections.fill = el.value
+    onAddTxt(el.value, gCurrSelections.stroke, true)
+  }
+  
+  function onChangeBorder(el) {
+    gCurrSelections.stroke = el.value
+    onAddTxt(gCurrSelections.fill, el.value, true)
+  }
+
+  function onChangeFontSize(diff) {
+    const fontSizeEl = document.querySelector('.font-size span')
+    let fontSize = parseInt(fontSizeEl.innerText) + diff
+    if (fontSize < 10) fontSize = 10
+    fontSizeEl.innerText = fontSize
+    onAddTxt(gCurrSelections.fill, gCurrSelections.stroke, true)
+  }
+  
